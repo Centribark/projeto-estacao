f4cc4ee5abb2f6306220cb0e37607dbb941f8ae2
Germano <ismaelgermano@icloud.com>
2018-03-03 17:43:51 -0300

    area de indique sua localizacao pronta

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// alert("Olá Mundo!");
+
+let numero = 5;
+
+if(numero < 5){
+console.log(`O número é menor do que 5`);
+}
+else if(numero == 10){
+console.log(`O número é igual a 10!`);
+}
+else {
+console.log(`O número não é menor que cinco e nem é igual a 10.`);
+}
